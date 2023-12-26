6e2cedbbfe870ded09b198c826ae1cdbbb16e37d
Dima Protsak <protsack.dima25@gmail.com>
2023-12-26 21:41:54 +0200

    PLAYTINI-1: Removes storyboard

--- PlaytiniTestApp/PlaytiniTestApp/PlaytiniViewController.swift
+++ PlaytiniTestApp/PlaytiniTestApp/PlaytiniViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PlaytiniViewController.swift
 //  PlaytiniTestApp
 //
 //  Created by Дмитрий Процак on 26.12.2023.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class PlaytiniViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
-
 }
 
